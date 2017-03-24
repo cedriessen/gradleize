af558f2072850302e860c6c07dbe79e8b1399d24
Christoph Driessen <cdriessen@extron.com>
2017-03-24 12:09:57 +0100

    add some documentation

--- lib/gradleize/dependency.ex
+++ lib/gradleize/dependency.ex
@@ -6,10 +6,18 @@ defmodule Gradleize.Dependency do
   @enforce_keys [:group_id, :artifact_id]
   defstruct [:group_id, :artifact_id, :version, :scope]
 
+  @doc """
+  Check if `field` is defined.
+  A field is considered to be defined if it is neither `nil` nor an empty string.
+  """
   def defined?(dependency, field) do
     not(defined?(dependency, field))
   end
 
+  @doc """
+  Check if `field` is undefined.
+  A field is considered to be defined if it is neither `nil` nor an empty string.
+  """
   def undefined?(dependency, field) do
     Map.get(dependency, field) in ["", nil]
   end
