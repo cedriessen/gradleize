fc06d494765158f75de28f5ad41afa6475383ae4
Christoph Driessen <cdriessen@extron.com>
2017-04-08 19:02:23 +0200

    finalize module dependency hierarchy resolver

--- lib/gradleize/module_dependency_hierarchy.ex
+++ lib/gradleize/module_dependency_hierarchy.ex
@@ -1,21 +1,26 @@
 defmodule Gradleize.ModuleDependencyHierarchy do
-  @moduledoc false
+  @moduledoc """
+  Create a project modules dependency hierarchy.
+  """
 
   alias Gradleize.ModuleDependencyHierarchy.Parser
+  alias Gradleize.Shell
+
+  @type module_name :: binary
+  @type mod :: {module_name, resolved_dependencies :: [module_name], unresolved_dependencies :: [module_name]}
 
   def build_hierarchy(dependencies) do
-    # {module, resolved, unresolved}
+    # [{module, resolved, unresolved}]
     modules =
       dependencies
       |> Parser.parse
       |> Enum.map(fn {module, dependencies} -> {module, [], dependencies} end)
-    case resolve([], modules, [], length(modules)) do
-      {:ok, resolved} ->
-        print_modules(resolved)
-      {:error, resolved, unresolved} ->
-        print_modules(resolved)
-        print_modules(unresolved)
-    end
+    # start resolution
+    {resolved, unresolved} = resolve([], modules, [], length(modules))
+    Shell.ansi [?\n, :green_background, :black, "FULLY RESOLVED"]
+    print_modules(resolved)
+    Shell.ansi [?\n, :red_background, :black, "UNRESOLVED"]
+    print_modules(unresolved)
   end
 
   defp print_modules(modules) do
@@ -23,61 +28,71 @@ defmodule Gradleize.ModuleDependencyHierarchy do
     |> Enum.each(&print_module/1)
   end
 
-  defp print_module({module, [], []}), do: IO.puts(module)
+  @spec print_module(mod) :: any
+  defp print_module({module, [], []}), do: Shell.text(module)
   defp print_module({module, resolved, unresolved}) do
-    IO.puts(module)
+    Shell.text [?\n, module]
     resolved
-    |> Enum.each(& IO.puts(" + " <> &1))
+    |> Enum.sort
+    |> Enum.each(& Shell.ansi [:green, " + ", &1])
     unresolved
-    |> Enum.each(& IO.puts(" - " <> &1))
+    |> Enum.sort
+    |> Enum.each(& Shell.ansi [:red, " - ", &1])
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  # resolved, unresolved, take_over, modules_count
-  def resolve(m_resolved, [], [], _) do
-    {:ok, m_resolved |> Enum.reverse}
+  @doc """
+  Resolve module dependendies and return a sorted list which shows the dependency hierarchy
+  of the modules.
+
+  ## Params
+  - `resolved`     - list of modules whose dependencies have been fully resolved
+  - `unresolved`   - list of modules with unresolved dependencies
+  - `again`        - list of modules which cannot be resolved in the current resolution iteration.
+                     Try to resolve those in the next round.
+  - `count`        - number of unresolved modules the current resolution iteration has been started with
+
+  Return a tuple of module lists `{fully_resolved, unresolved}`.
+  """
+  @spec resolve([mod], [mod], [mod], integer) :: {[mod], [mod]}
+  def resolve(resolved, unresolved, again, count)
+  def resolve(mrs, [], [], _) do
+    {mrs |> Enum.reverse, []}
   end
-  def resolve(
-    m_resolved,
-    [{_, _, []} = m | m_rest], # no more unresolved dependencies
-    take_over,
-    count)
-  do
-    resolve([m | m_resolved], m_rest, take_over, count)
+  # match module in unresolved with no more unresolved dependencies
+  def resolve(mrs, [{_, _, []} = mu | mus], mas, c) do
+    resolve([mu | mrs], mus, mas, c)
   end
-  def resolve(
-    m_resolved,
-    [{m_name, d_resolved, [d | d_rest]} = m | m_rest],
-    take_over,
-    count)
-  do
-    if contains?(m_resolved, d) do
-      resolve(
-        m_resolved,
-        [{m_name, [d | d_resolved], d_rest} | m_rest], # move dependency to resolved and try on
-        take_over,
-        count)
+  # inspect first unresolved dependency
+  def resolve(mrs, [{mn, drs, [du | dus]} | mus], mas, c) do
+    if mrs |> contains?(du) do
+      # the list of resolved modules contains the unresolved dependency
+      # -> move dependency to resolved and try next unresolved dependency
+      resolve(mrs, [{mn, [du | drs], dus} | mus], mas, c)
     else
-      resolve(
-        m_resolved,
-        m_rest,
-        [m | take_over], # move module to take_over for a later try
-        count)
+      # Move module to 'again' list for a later try.
+      # In order to try to resolve as much as possible move the unresolved dependency to the end
+      # of dependency list.
+      resolve(mrs, mus, [{mn, drs, dus ++ [du]} | mas], c)
     end
   end
-  def resolve(resolved, [], take_over, c) when length(take_over) < c do
-    resolve(resolved, take_over, [], length(take_over))
+  # end of iteration, still some module to try again, so start a new iteration
+  def resolve(mrs, [], mas, c) when length(mas) < c do
+    resolve(mrs, mas, [], length(mas))
   end
-  def resolve(resolved, [], take_over, _) do
-    {:error, resolved |> Enum.reverse, take_over}
+  # end of iteration, with still some modules to try again but the last iteration did
+  # not reduce the amount of unresolved modules so we have some unresolvable ones :(
+  def resolve(mrs, [], mas, _) do
+    {mrs |> Enum.reverse, mas}
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   # check if dependency (module name) is contained in the list of module tuples
+  @spec contains?([mod], module_name) :: boolean
   defp contains?(modules, dependency) do
     modules
-    |> Enum.any?(fn {m, _, _} -> m == dependency end)
+    |> Enum.any?(fn {mn, _, _} -> mn == dependency end)
   end
 end
