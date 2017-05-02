27bc82a80f9fc8ca495dc6e0daa08a8a67f5d2b1
Christoph Driessen <cdriessen@extron.com>
2017-05-02 13:57:23 +0200

    fix test

--- test/gradleize_test.exs
+++ test/gradleize_test.exs
@@ -1,28 +1,16 @@
 defmodule GradleizeTest do
   use ExUnit.Case
-  import ExUnit.CaptureIO
 
   doctest Gradleize
 
   test "create library definitions from main pom" do
-    test = fn ->
+    output =
       Gradleize.Opencast.main_pom()
       |> Gradleize.Dependencies.create_library_definitions
-    end
+      |> :erlang.iolist_to_binary
 
-    output = capture_io(test)
     assert output =~ "libraries.commons_lang3 = 'org.apache.commons:commons-lang3:3.4'"
     assert output =~ "libraries.cxf_rt_frontend_jaxrs = \"org.apache.cxf:cxf-rt-frontend-jaxrs:${versions.cxf}\""
     assert output =~ "${versions.osgi_core}"
-  end
-
-  test "create dependency definitions for matterhorn-common" do
-    test = fn ->
-      "matterhorn-common"
-      |> Gradleize.Opencast.module_pom
-      |> Gradleize.Dependencies.create_module_dependencies
-    end
-    test.()
-#    assert capture_io(test) =~ "compile libraries.guava"
   end
 end
